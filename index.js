ff83051a16fd0759b832712226bacac01f80a6d6
Jesse Jackson <jsejcksn@users.noreply.github.com>
2016-05-11 07:40:13 -0500

    Add index.js, Update package.json

---
+++ index.js
@@ -0,0 +1,50 @@
+var data = require('./private.js'),
+  ping = require('ping'),
+  postJson = require('post-json'),
+  moment = require('moment'),
+  humanizeDuration = require('humanize-duration');
+
+var hosts = [data.server],
+  url = data.url.jesse,
+  body = {},
+  mark = moment();
+
+function loopFail() { // Loop until connection succeeds, then switch to loopSuccess
+  hosts.forEach(function(host) {
+    ping.sys.probe(host, function(isAlive) { // Ping host
+      if (isAlive) {
+        body.value1 = "Modem is back online"; // Change value of the first key in the webhook JSON body object
+        console.log(body.value1);
+        body.value2 = 'Downtime was ' + humanizeDuration(moment.duration(moment().diff(moment(mark)))); // Change value of the second key in the webhook JSON body object
+        console.log(body.value2);
+        mark = moment();
+        postJson(url, body, function(err, result) {}); // Trigger IFTTT webhook
+        loopSuccess();
+      } else {
+        console.log(moment().format() + ' ' + host + ' did not respond');
+        setTimeout(loopFail, (30 * 1000)); // Run loopFail in 30s
+      }
+    });
+  });
+}
+
+function loopSuccess() { // Loop until connection fails, then switch to loopFail
+  hosts.forEach(function(host) {
+    ping.sys.probe(host, function(isAlive) { // Ping host
+      if (isAlive) {
+        console.log(moment().format() + ' ' + host + ' is online');
+        setTimeout(loopSuccess, (30 * 1000)); // Run loopSuccess in 30s
+      } else {
+        body.value1 = "Modem is not responding"; // Change value of the first key in the webhook JSON body object
+        console.log(body.value1);
+        body.value2 = 'Uptime was ' + humanizeDuration(moment.duration(moment().diff(moment(mark)))); // Change value of the second key in the webhook JSON body object
+        console.log(body.value2);
+        mark = moment();
+        postJson(url, body, function(err, result) {}); // Trigger IFTTT webhook
+        loopFail();
+      }
+    });
+  });
+}
+
+loopFail();
