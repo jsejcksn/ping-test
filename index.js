31e8dd542ad82e45b60bc0d3c1d10c8cda032dcb
Jesse Jackson <jsejcksn@users.noreply.github.com>
2016-05-21 18:07:36 -0500

    Make a lot of changes (sorry; see description)
    - Add copious comments
    - Modularize functions; closes #4
    - Move webhook variables into an object
    - Add ‘count’ var for double-testing status prior to taking action

--- index.js
+++ index.js
@@ -25,23 +25,29 @@ var privateData = require('./private.js'),
   });
 
 // Variables
-var hosts = [privateData.server],
-  url = privateData.url.jesse,
-  body = {},
-  mark = moment();
+var hosts = [privateData.server], // This array contains only one object because this app is intended for pinging only one device
+  webhook = {
+    'url': privateData.url, // Webhook URL
+    'body': {} // POST body for webhook
+  },
+  loopCount = 0,
+  mark = moment(); // Set initial moment for getting diffs
 
-isDown();
+isDown(); // Initiate with the assumption that the device is offline; causes first webhook event to be about device being online
 
 function isDown() { // Loop until connection succeeds, then switch to isUp
   hosts.forEach(function(host) {
     ping.sys.probe(host, function(isAlive) { // Ping host
       if (isAlive) {
-        body.value1 = "Modem is back online"; // Change value of the first key in the webhook JSON body object
-        body.value2 = 'Downtime was ' + shortEnglishHumanizer(moment.duration(moment().diff(moment(mark)))); // Change value of the second key in the webhook JSON body object
-        console.error(moment().format(), body.value1 + ', ' + body.value2);
-        mark = moment();
-        postJson(url, body, function(err, result) {}); // Trigger IFTTT webhook
-        isUp();
+        loopCount++;
+        if (loopCount > 1) { // Double-check before taking action
+          webhook.body.value1 = "Modem is back online"; // Change value of the first key in the webhook JSON body object
+          webhook.body.value2 = 'Downtime was ' + shortEnglishHumanizer(moment.duration(moment().diff(moment(mark)))); // Change value of the second key in the webhook JSON body object
+          logPost();
+          isUp();
+        } else {
+          isDown();
+        }
       } else {
         console.log(moment().format() + ' ' + host + ' did not respond');
         setTimeout(isDown, (30 * 1000)); // Run isDown in 30s
@@ -57,13 +63,23 @@ function isUp() { // Loop until connection fails, then switch to isDown
         console.log(moment().format() + ' ' + host + ' is online');
         setTimeout(isUp, (30 * 1000)); // Run isUp in 30s
       } else {
-        body.value1 = "Modem is not responding"; // Change value of the first key in the webhook JSON body object
-        body.value2 = 'Uptime was ' + shortEnglishHumanizer(moment.duration(moment().diff(moment(mark)))); // Change value of the second key in the webhook JSON body object
-        console.error(moment().format(), body.value1 + ', ' + body.value2);
-        mark = moment();
-        postJson(url, body, function(err, result) {}); // Trigger IFTTT webhook
-        isDown();
+        loopCount++;
+        if (loopCount > 1) { // Double-check before taking action
+          webhook.body.value1 = "Modem is not responding"; // Change value of the first key in the webhook JSON body object
+          webhook.body.value2 = 'Uptime was ' + shortEnglishHumanizer(moment.duration(moment().diff(moment(mark)))); // Change value of the second key in the webhook JSON body object
+          logPost();
+          isDown();
+        } else {
+          isUp();
+        }
       }
     });
   });
+}
+
+function logPost() {
+  mark = moment(); // Reset moment for getting diffs
+  console.error(moment().format(), webhook.body.value1 + ', ' + webhook.body.value2); // Log status change
+  postJson(webhook.url, webhook.body, function(err, result) {}); // Trigger webhook
+  loopCount = 0;
 }
