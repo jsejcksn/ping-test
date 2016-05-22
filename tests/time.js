6828586c792be204178574004a77b781842d6081
Jesse Jackson <jsejcksn@users.noreply.github.com>
2016-05-22 04:31:46 -0500

    (Semi)Standard-ize style

--- tests/time.js
+++ tests/time.js
@@ -1,21 +1,21 @@
-var moment = require('moment'),
-  humanizeDuration = require('humanize-duration');
+var moment = require('moment');
+var humanizeDuration = require('humanize-duration');
 
-var mark = moment(),
-  mark2 = (1 * (365.25 * 24 * 60 * 60 * 1000)) + (1 * (30.4375 * 24 * 60 * 60 * 1000)) + (1 * (7 * 24 * 60 * 60 * 1000)) + (1 * (24 * 60 * 60 * 1000)) + (1 * (60 * 60 * 1000)) + (1 * (60 * 1000)) + (1 * (1000)),
-  mark3 = (3 * (365.25 * 24 * 60 * 60 * 1000)) + (10 * (30.4375 * 24 * 60 * 60 * 1000)) + (2 * (7 * 24 * 60 * 60 * 1000)) + (21 * (24 * 60 * 60 * 1000)) + (19 * (60 * 60 * 1000)) + (45 * (60 * 1000)) + (17.5 * (1000));
+var mark = moment();
+var mark2 = (1 * (365.25 * 24 * 60 * 60 * 1000)) + (1 * (30.4375 * 24 * 60 * 60 * 1000)) + (1 * (7 * 24 * 60 * 60 * 1000)) + (1 * (24 * 60 * 60 * 1000)) + (1 * (60 * 60 * 1000)) + (1 * (60 * 1000)) + (1 * (1000));
+var mark3 = (3 * (365.25 * 24 * 60 * 60 * 1000)) + (10 * (30.4375 * 24 * 60 * 60 * 1000)) + (2 * (7 * 24 * 60 * 60 * 1000)) + (21 * (24 * 60 * 60 * 1000)) + (19 * (60 * 60 * 1000)) + (45 * (60 * 1000)) + (17.5 * (1000));
 var shortEnglishHumanizer = humanizeDuration.humanizer({
   language: 'shortEn',
   languages: {
     shortEn: {
-      y: function() { return 'y'; },
-      mo: function() { return 'mo'; },
-      w: function() { return 'w'; },
-      d: function() { return 'd'; },
-      h: function() { return 'h'; },
-      m: function() { return 'm'; },
-      s: function() { return 's'; },
-      ms: function() { return 'ms'; },
+      y: function () { return 'y'; },
+      mo: function () { return 'mo'; },
+      w: function () { return 'w'; },
+      d: function () { return 'd'; },
+      h: function () { return 'h'; },
+      m: function () { return 'm'; },
+      s: function () { return 's'; },
+      ms: function () { return 'ms'; }
     }
   },
   delimiter: ' ',
@@ -24,7 +24,7 @@ var shortEnglishHumanizer = humanizeDuration.humanizer({
   units: ['y', 'mo', 'w', 'd', 'h', 'm']
 });
 
-function display() {
+function display () {
   console.log(shortEnglishHumanizer(moment.duration(moment().diff(moment(mark))))); // Get difference of duration (in ms) between marked moment and current moment, then display in human-readable format *with precision*.
 }
 
