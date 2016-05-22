d423e2fec7b34485ba14c61d983a538673b1598b
Jesse Jackson <jsejcksn@users.noreply.github.com>
2016-05-22 03:44:50 -0500

    Style to conform to JS (Semi)Standard Style

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 // Modules
-var privateData = require('./private.js'),
-  ping = require('ping'),
-  postJson = require('post-json'),
-  moment = require('moment'),
-  humanizeDuration = require('humanize-duration'),
-  shortEnglishHumanizer = humanizeDuration.humanizer({
-    language: 'shortEn',
-    languages: {
-      shortEn: {
-        y: function() { return 'y'; },
-        mo: function() { return 'mo'; },
-        w: function() { return 'w'; },
-        d: function() { return 'd'; },
-        h: function() { return 'h'; },
-        m: function() { return 'm'; },
-        s: function() { return 's'; },
-        ms: function() { return 'ms'; },
-      }
-    },
-    delimiter: ' ',
-    round: true,
-    spacer: '',
-    units: ['y', 'mo', 'w', 'd', 'h', 'm']
-  });
+var privateData = require('./private.js');
+var ping = require('ping');
+var postJson = require('post-json');
+var moment = require('moment');
+var humanizeDuration = require('humanize-duration');
+var shortEnglishHumanizer = humanizeDuration.humanizer({
+  language: 'shortEn',
+  languages: {
+    shortEn: {
+      y: function () { return 'y'; },
+      mo: function () { return 'mo'; },
+      w: function () { return 'w'; },
+      d: function () { return 'd'; },
+      h: function () { return 'h'; },
+      m: function () { return 'm'; },
+      s: function () { return 's'; },
+      ms: function () { return 'ms'; }
+    }
+  },
+  delimiter: ' ',
+  round: true,
+  spacer: '',
+  units: ['y', 'mo', 'w', 'd', 'h', 'm']
+});
 
 // Variables
-var host = privateData.server,
-  webhook = {
-    'url': privateData.url, // Webhook URL
-    'body': {} // POST body for webhook
-  },
-  loopCount = 0,
-  mark = moment(); // Set initial moment for getting diffs
+var host = privateData.server;
+var webhook = {
+  'url': privateData.url, // Webhook URL
+  'body': {} // POST body for webhook
+};
+var loopCount = 0;
+var mark = moment(); // Set initial moment for getting diffs
 
 isDown(); // Initiate with the assumption that the device is offline; causes first webhook event to be about device being online
 
-function isDown() { // Loop until connection succeeds, then switch to isUp
-  ping.sys.probe(host, function(isAlive) { // Ping host
+function isDown () { // Loop until connection succeeds, then switch to isUp
+  ping.sys.probe(host, function (isAlive) { // Ping host
     if (isAlive) {
       loopCount++;
       if (loopCount > 1) { // Double-check before taking action
-        webhook.body.value1 = "Modem is back online"; // Change value of the first key in the webhook JSON body object
+        webhook.body.value1 = 'Modem is back online'; // Change value of the first key in the webhook JSON body object
         webhook.body.value2 = 'Downtime was ' + shortEnglishHumanizer(moment.duration(moment().diff(moment(mark)))); // Change value of the second key in the webhook JSON body object
         logPost();
         isUp();
@@ -54,15 +54,15 @@ function isDown() { // Loop until connection succeeds, then switch to isUp
   });
 }
 
-function isUp() { // Loop until connection fails, then switch to isDown
-  ping.sys.probe(host, function(isAlive) { // Ping host
+function isUp () { // Loop until connection fails, then switch to isDown
+  ping.sys.probe(host, function (isAlive) { // Ping host
     if (isAlive) {
       console.log(moment().format() + ' ' + host + ' is online');
       setTimeout(isUp, (30 * 1000)); // Run isUp in 30s
     } else {
       loopCount++;
       if (loopCount > 1) { // Double-check before taking action
-        webhook.body.value1 = "Modem is not responding"; // Change value of the first key in the webhook JSON body object
+        webhook.body.value1 = 'Modem is not responding'; // Change value of the first key in the webhook JSON body object
         webhook.body.value2 = 'Uptime was ' + shortEnglishHumanizer(moment.duration(moment().diff(moment(mark)))); // Change value of the second key in the webhook JSON body object
         logPost();
         isDown();
@@ -73,9 +73,13 @@ function isUp() { // Loop until connection fails, then switch to isDown
   });
 }
 
-function logPost() {
+function logPost () {
   mark = moment(); // Reset moment for getting diffs
   console.error(moment().format(), webhook.body.value1 + ', ' + webhook.body.value2); // Log status change
-  postJson(webhook.url, webhook.body, function(err, result) {}); // Trigger webhook
+  postJson(webhook.url, webhook.body, function (err, result) {  // Trigger webhook
+    if (err) {
+      console.error(err);
+    }
+  });
   loopCount = 0;
 }
