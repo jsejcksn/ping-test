02053e83d0e51ed7cf416c2ad4d4c37f1b63142b
Jesse Jackson <jsejcksn@users.noreply.github.com>
2016-05-22 05:37:46 -0500

    Stringify keys

--- tests/logging.js
+++ tests/logging.js
@@ -1,8 +1,8 @@
-var logData = [{date:'2016-05-15T03:35:29+00:00',reachable:true,timeSinceLast:'2m'},
-{date:'2016-05-15T03:35:31+00:00',reachable:false,timeSinceLast:'0m'},
-{date:'2016-05-15T03:36:04+00:00',reachable:true,timeSinceLast:'1m'},
-{date:'2016-05-15T03:36:06+00:00',reachable:false,timeSinceLast:'0m'},
-{date:'2016-05-15T03:37:12+00:00',reachable:true,timeSinceLast:'1m'}];
+var logData = [{'date':'2016-05-15T03:35:29+00:00','reachable':true,'timeSinceLast':'2m'},
+{'date':'2016-05-15T03:35:31+00:00','reachable':false,'timeSinceLast':'0m'},
+{'date':'2016-05-15T03:36:04+00:00','reachable':true,'timeSinceLast':'1m'},
+{'date':'2016-05-15T03:36:06+00:00','reachable':false,'timeSinceLast':'0m'},
+{'date':'2016-05-15T03:37:12+00:00','reachable':true,'timeSinceLast':'1m'}];
 
 // console.log(logData.length);
 
